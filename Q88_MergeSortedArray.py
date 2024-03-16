3d26096bc37a2368e4c44ece5eeea10c3a4c7e9b
Clancy Lin <poerevan9@gmail.com>
2024-03-16 23:06:16 +0800

    Upload Q88

---
+++ Q88_MergeSortedArray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+        nums1[:] = sorted(nums1[:m] + nums2[:n])
+
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
+n = 3
+
+sol = Solution()
+sol.merge(nums1, m, nums2, n)
+print(nums1)
