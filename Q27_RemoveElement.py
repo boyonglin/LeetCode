5fb90d2eecc5ed89377adcf5698358976319f610
Clancy Lin <poerevan9@gmail.com>
2024-03-19 10:31:09 +0800

    Submit Q26

--- Q27_RemoveElement.py
+++ Q27_RemoveElement.py
@@ -3,6 +3,7 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
         k = 0
+
         for i in range(len(nums)):
             if nums[i] != val:
                 nums[k] = nums[i]
