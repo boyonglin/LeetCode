c5bf6455c247342522ec2acc9be9d12a1d944727
Clancy Lin <poerevan9@gmail.com>
2024-03-31 18:24:30 +0800

    Submit Q121

---
+++ Top Interview 150/Q121_BestTimetoBuyandSellStock.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+        for i in range(len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
+
+        return profit
+
+# Testcase
+prices = [7,1,5,3,6,4]
+
+sol = Solution()
+print(sol.maxProfit(prices))
