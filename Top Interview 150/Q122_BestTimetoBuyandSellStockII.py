34162d6c7d52184bd54399dddd8af97c72d237c4
Clancy Lin <poerevan9@gmail.com>
2024-04-01 16:27:51 +0800

    Update Q122

--- Top Interview 150/Q122_BestTimetoBuyandSellStockII.py
+++ Top Interview 150/Q122_BestTimetoBuyandSellStockII.py
@@ -3,19 +3,14 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        all_profit = 0
         buy = prices[0]
 
         for i in range(1, len(prices)):
-            if prices[i] < buy:
-                buy = prices[i]
-            elif prices[i] - buy > profit:
-                profit = prices[i] - buy
-                all_profit += profit
-                buy = prices[i]
-                profit = 0
+            if prices[i] > buy:
+                profit += prices[i] - buy
+            buy = prices[i]
 
-        return all_profit
+        return profit
 
 # Testcase
 prices = [7,1,5,3,6,4]
