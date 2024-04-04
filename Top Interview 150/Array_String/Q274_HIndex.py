5239bb196a4c3d5d61a067f74b5d5decf8546ad3
Clancy Lin <poerevan9@gmail.com>
2024-04-04 19:15:36 +0800

    Submit Q274

---
+++ Top Interview 150/Array_String/Q274_HIndex.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        h = 0
+        citations[:] = sorted(citations, reverse=True)
+        for i, c in enumerate(citations, start=1):
+            if i < c:
+                h += 1
+            elif i == c:
+                return i
+        return h
+
+# Testcase
+citations = [3,0,6,1,5]
+
+sol = Solution()
+print(sol.hIndex(citations))
