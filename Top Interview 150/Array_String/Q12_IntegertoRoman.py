78dc711538b96dacc2a461310797506ed62e0753
Clancy Lin <poerevan9@gmail.com>
2024-09-08 22:48:59 +0800

    Submit Q12

---
+++ Top Interview 150/Array_String/Q12_IntegertoRoman.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        int_dict = {
+            1: "I", 4: "IV", 5: "V", 9: "IX",
+            10: "X", 40: "XL", 50: "L", 90: "XC",
+            100: "C", 400: "CD", 500: "D", 900: "CM",
+            1000: "M"
+        }
+
+        roman = ""
+
+        for i in sorted(int_dict.keys(), reverse=True):
+            while num >= i:
+                roman += int_dict[i]
+                num -= i
+
+        return roman
+
+# Testcase
+num = 1994
+
+sol = Solution()
+print(sol.intToRoman(num))
