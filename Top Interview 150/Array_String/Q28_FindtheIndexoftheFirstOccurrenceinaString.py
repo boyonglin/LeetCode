1cd0f21b23937ae6af4eb0f4526183c0eea28bbe
Clancy Lin <poerevan9@gmail.com>
2024-09-11 12:02:34 +0800

    Submit Q28

---
+++ Top Interview 150/Array_String/Q28_FindtheIndexoftheFirstOccurrenceinaString.py
@@ -0,0 +1,23 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle:
+            return 0
+
+        for i in range(len(haystack)):
+            if haystack[i] == needle[0]:
+                if haystack[i:i + len(needle)] == needle:
+                    return i
+
+        return -1
+
+        # for i in range(len(haystack) - len(needle) + 1):
+        #     if haystack[i:i + len(needle)] == needle:
+        #         return i
+
+
+# Testcase\
+haystack = "sadbutsad"
+needle = "sad"
+
+sol = Solution()
+print(sol.strStr(haystack, needle))
