06b36651e4ac213c4fb3bbfa3f56f8446437a93d
Clancy Lin <poerevan9@gmail.com>
2024-09-23 11:48:36 +0800

    Update Q49

--- Top Interview 150/Hashmap/Q49_GroupAnagrams.cpp
+++ Top Interview 150/Hashmap/Q49_GroupAnagrams.cpp
@@ -9,12 +9,21 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // map<vector<int>, vector<string>> unimap;
         map<string, vector<string>> unimap;
         vector<vector<string>> res;
 
         for (string s : strs) {
+            // character frequency counting O(N * M)
+            // vector<int> key(26, 0);
+            // for (char c : s) {
+            //     key[c - 'a']++;
+            // }
+
+            // sort the characters O(N * M log M)
             string key = s;
             sort(key.begin(), key.end());
+
             unimap[key].push_back(s);
         }
 
