82449c632907683a5a8851a65bd3c4e2ec3a3028
Clancy Lin <poerevan9@gmail.com>
2024-09-24 14:57:34 +0800

    Submit Qtmf

---
+++ Interview/Qtmf_ChangingSequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def changingSequence(self, array):
+        head = array[0]
+        next = array[1]
+
+        if next > head:
+            for i in range(1, len(array)):
+                if array[i] <= array[i-1]:
+                    return i - 1
+        else:
+            for i in range(1, len(array)):
+                if array[i] >= array[i-1]:
+                    return i - 1
+
+        return -1
+
+array = [1,2,4,8,4,2]
+sol = Solution()
+print(sol.changingSequence(array))
