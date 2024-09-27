a566107e4ff45db9dfd0be5172916b0a38a71325
Clancy Lin <poerevan9@gmail.com>
2024-09-27 23:24:24 +0800

    Submit Q150

--- Top Interview 150/Stack/Q150_EvaluateReversePolishNotation.py
+++ Top Interview 150/Stack/Q150_EvaluateReversePolishNotation.py
@@ -3,13 +3,33 @@ from typing import List
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
         num_stack = []
+        res = 0
 
-        for token in tokens:
-            if token not in "+-*/":
-                num_stack.append(int(token))
+        if len(tokens) == 1:
+            return int(tokens[0])
+
+        for t in tokens:
+            if t not in '+-*/':
+                num_stack.append(int(t))
+            else:
+                a = num_stack.pop()
+                b = num_stack.pop()
+
+                if t == '+':
+                    res = b + a
+                elif t == '-':
+                    res = b - a
+                elif t == '*':
+                    res = b * a
+                else:
+                    res = int(b / a)
+
+                num_stack.append(res)
+
+        return res
 
 # Testcases
-tokens = ["2","1","+","3","*"]
+tokens = ["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
 
 sol = Solution()
 print(sol.evalRPN(tokens))
