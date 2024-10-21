52e04c1a96f37c183447124dc38d220adf74d6b8
Clancy Lin <poerevan9@gmail.com>
2024-10-21 20:31:18 +0800

    Submit Throttle

---
+++ JS30/Promises and Time/Q2676_Throttle.js
@@ -0,0 +1,19 @@
+var throttle = function (fn, delay) {
+    let timer = null;
+
+    return (...args) => {
+        if (timer) return;
+
+        timer = setTimeout(() => {
+            fn(...args);
+            timer = null;
+        }, delay);
+    };
+}
+
+const throttled = throttle(() => console.log("throttle"), 100);
+
+// if keep scrolling, the console will keep logging "throttle" every 100ms
+window.addEventListener("scroll", () => {
+    updateThrottleText();
+});
